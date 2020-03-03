4ee3a5b4124eaf9a063be89c721a0f23ca141bc7
perebeynos.roma@gmail.com <perebeynos.roma@gmail.com>
2020-03-03 14:28:57 +0200

    generate a JWT token in dotnet core 3

--- Server/Controllers/HomeController.cs
+++ Server/Controllers/HomeController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Server.Controllers
 {
@@ -20,8 +24,38 @@ namespace Server.Controllers
         }
         public IActionResult Authenticate()
         {
-            return RedirectToAction("Index");
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "some_id"),
+                new Claim("granny",  "cookie"),
+            };
+            var secretBytes = Encoding.UTF8.GetBytes(Constants.Secret);
 
+            var key = new SymmetricSecurityKey(secretBytes);
+
+            var algoritm = SecurityAlgorithms.HmacSha256;
+
+            var siningCredentials = new SigningCredentials(key, algoritm);
+
+            var token = new JwtSecurityToken(
+                Constants.Issuer,
+                Constants.Audiance,
+                claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddHours(1),
+                siningCredentials);
+
+            var tokenJson = new JwtSecurityTokenHandler().WriteToken(token);
+
+
+            return Ok(new { acces_token = tokenJson });
+
+        }
+        public IActionResult Decode(string part)
+        {
+          var bytes =   Convert.FromBase64String(part);
+
+            return Ok(Encoding.UTF8.GetString(bytes));
         }
     }
 }
